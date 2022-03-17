4c667c9d94c4096452ec85d6b891bf4394ef9eec
henry <63601449+HenryBass@users.noreply.github.com>
2022-03-17 13:30:28 +0000

    idk

--- lang.py
+++ lang.py
@@ -5,54 +5,68 @@ ops = ["+", "-", "="]
 
 a = 0
 r = True
-d = 0
+data = int(len(mem)/2)
 pointer = 0
 
 inst = "="
 
 exit = False
 
-code = list("vABCDEFGB*!")
+code = list("^A$B@%!")
 
 for i in range(0, len(code)):
-	mem[i] = code[i]
+ mem[i] = code[i]
 
 # first 64 is code
 # last 64 is mem
 
 def execute():
-	global inst
-	global a
-	global mem
-	global r
-	global exit
-	global pointer
+ global inst
+ global a
+ global mem
+ global r
+ global exit
+ global pointer
+ global data
 
-	p = mem[pointer]
-	if p in base32:
-		if inst == "=":
-			a = base32.index(p)
-		elif inst == "+":
-			a += base32.index(p)
-		else:
-			a -= base32.index(p)
-	elif p in ops:
-		inst = str(p)
-	elif p == "^":
-		r = False
-	elif p == "v":
-		r = True
-	elif p == "*":
-		if r:
-			a = base32.index(pointer)
-		else:
-			pointer = a
-	elif p == "!":
-		exit = True
-	print(pointer)
-
-
+ p = mem[pointer]
+  
+ if p in base32:
+  if inst == "=":
+   a = base32.index(p)
+  elif inst == "+":
+   a += base32.index(p)
+  else:
+   a -= base32.index(p)
+ elif p in ops:
+  inst = str(p)
+ elif p == "^":
+  r = False
+ elif p == "v":
+  r = True
+ elif p == "*":
+  if r:
+   a = pointer
+  else:
+   pointer = a
+ elif p == "%":
+  if r:
+   a = ord(input()[0])
+  else:
+   print(chr(a))
+ elif p == "@":
+  if r:
+   a = data
+  else:
+   data = a
+ elif p == "$":
+  if r:
+   a = mem[data]
+  else:
+   mem[data] = a
+ elif p == "!":
+  exit = True
 
 while exit == False:
-	execute()
-	pointer += 1
+ execute()
+ pointer += 1
